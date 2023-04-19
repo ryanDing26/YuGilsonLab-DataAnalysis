f93bef3eaf935ff025fc3d54e2612d96e3125431
Ryan Ding <dingryan2@gmail.com>
2023-04-19 16:02:23 -0700

    added center functino
    added code to calculate the center of mass of a ligand

--- RMSD.sh
+++ RMSD.sh
@@ -3,4 +3,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Change bash script directory
+# CL args: structure
 python RMSD.py "/Users/yashravipati/Downloads/PDBBind_processed/$1/$1_docked.pdbqt" "/Users/yashravipati/Downloads/$1.pdb"
